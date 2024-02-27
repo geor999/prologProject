f32358a8e45147b1153ea9c6e830266885258ee8
geor999 <72676920+geor999@users.noreply.github.com>
2024-02-27 16:45:56 +0200

    Prolog project

---
+++ 3691-3688.pl
@@ -0,0 +1,330 @@
+%FOITHTES: ΔΗΜΗΤΡΙΟΣ ΓΕΩΡΓΑΝΤΗΣ 3691 ΞΕΝΟΦΩΝ ΠΛΕΜΑΤΙΑΣ 3688
+:- dynamic attends/2.
+
+attends(476, im218).
+attends(478, im218).
+attends(479, im218).
+attends(481, im218).
+attends(482, im218).
+attends(483, im218).
+attends(484, im218).
+attends(485, im218).
+attends(487, im218).
+attends(488, im218).
+attends(489, im218).
+attends(490, im218).
+attends(491, im218).
+attends(492, im218).
+attends(494, im218).
+attends(495, im218).
+attends(496, im218).
+attends(497, im218).
+attends(498, im218).
+attends(500, im218).
+attends(501, im218).
+attends(505, im218).
+attends(506, im218).
+attends(507, im218).
+attends(508, im218).
+attends(510, im218).
+attends(512, im218).
+attends(514, im218).
+attends(517, im218).
+attends(518, im218).
+attends(479, im217).
+attends(481, im217).
+attends(486, im217).
+attends(493, im217).
+attends(494, im217).
+attends(495, im217).
+attends(497, im217).
+attends(499, im217).
+attends(502, im217).
+attends(503, im217).
+attends(504, im217).
+attends(520, im217).
+attends(507, im217).
+attends(509, im217).
+attends(512, im217).
+attends(513, im217).
+attends(514, im217).
+attends(516, im217).
+attends(476, im204).
+attends(478, im204).
+attends(482, im204).
+attends(485, im204).
+attends(486, im204).
+attends(487, im204).
+attends(488, im204).
+attends(489, im204).
+attends(490, im204).
+attends(491, im204).
+attends(492, im204).
+attends(493, im204).
+attends(494, im204).
+attends(496, im204).
+attends(498, im204).
+attends(499, im204).
+attends(500, im204).
+attends(501, im204).
+attends(502, im204).
+attends(503, im204).
+attends(505, im204).
+attends(520, im204).
+attends(508, im204).
+attends(509, im204).
+attends(510, im204).
+attends(512, im204).
+attends(513, im204).
+attends(514, im204).
+attends(515, im204).
+attends(516, im204).
+attends(517, im204).
+attends(518, im204).
+attends(479, im210).
+attends(480, im210).
+attends(481, im210).
+attends(483, im210).
+attends(484, im210).
+attends(491, im210).
+attends(497, im210).
+attends(508, im210).
+attends(513, im210).
+attends(515, im210).
+attends(517, im210).
+attends(476, im209).
+attends(478, im209).
+attends(480, im209).
+attends(481, im209).
+attends(482, im209).
+attends(484, im209).
+attends(485, im209).
+attends(487, im209).
+attends(488, im209).
+attends(489, im209).
+attends(490, im209).
+attends(492, im209).
+attends(493, im209).
+attends(495, im209).
+attends(496, im209).
+attends(497, im209).
+attends(498, im209).
+attends(499, im209).
+attends(500, im209).
+attends(504, im209).
+attends(506, im209).
+attends(515, im209).
+attends(516, im209).
+attends(518, im209).
+attends(476, im216).
+attends(478, im216).
+attends(484, im216).
+attends(487, im216).
+attends(491, im216).
+attends(492, im216).
+attends(493, im216).
+attends(496, im216).
+attends(498, im216).
+attends(501, im216).
+attends(502, im216).
+attends(504, im216).
+attends(505, im216).
+attends(507, im216).
+attends(509, im216).
+attends(512, im216).
+attends(513, im216).
+attends(514, im216).
+attends(515, im216).
+attends(479, im214).
+attends(480, im214).
+attends(482, im214).
+attends(483, im214).
+attends(485, im214).
+attends(486, im214).
+attends(488, im214).
+attends(489, im214).
+attends(490, im214).
+attends(494, im214).
+attends(495, im214).
+attends(499, im214).
+attends(500, im214).
+attends(503, im214).
+attends(504, im214).
+attends(505, im214).
+attends(506, im214).
+attends(520, im214).
+attends(507, im214).
+attends(508, im214).
+attends(510, im214).
+attends(516, im214).
+attends(517, im214).
+attends(518, im214).
+attends(480, im212).
+attends(483, im212).
+attends(486, im212).
+attends(501, im212).
+attends(502, im212).
+attends(503, im212).
+attends(506, im212).
+attends(520, im212).
+attends(509, im212).
+attends(510, im212).
+
+%ΔΗΜΙΟΥΡΓΗΣΑΜΕ ΓΕΓΟΝΟΣ student ΤΟ ΟΠΟΙΟ ΧΡΗΣΙΜΟΠΟΙΕΙΤΑΙ ΓΙΑ ΤΟΝ ΕΛΕΓΧΟ ΦΟΙΤΗΤΗ-ΜΑΘΗΜΑΤΟΣ
+:- dynamic student/1.
+student(476).
+student(478).
+student(479).
+student(480).
+student(481).
+student(482).
+student(483).
+student(484).
+student(485).
+student(486).
+student(487).
+student(488).
+student(489).
+student(490).
+student(491).
+student(492).
+student(493).
+student(494).
+student(495).
+student(496).
+student(497).
+student(498).
+student(499).
+student(500).
+student(501).
+student(502).
+student(503).
+student(504).
+student(505).
+student(506).
+student(507).
+student(508).
+student(509).
+student(510).
+student(511).
+student(512).
+student(513).
+student(514).
+student(515).
+student(516).
+student(517).
+student(518).
+student(520).
+
+
+%ΔΗΜΙΟΥΡΓΗΣΑΜΕ ΓΕΓΟΝΟΣ subject ΤΟ ΟΠΟΙΟ ΧΡΗΣΙΜΟΠΟΙΕΙΤΑΙ ΓΙΑ ΕΛΕΓΧΟ ΜΑΘΗΜΑΤΟΣ-ΦΟΙΤΗΤΗ
+:- dynamic subject/1.
+
+
+subject(im204).
+subject(im209).
+subject(im210).
+subject(im212).
+subject(im214).
+subject(im216).
+subject(im217).
+subject(im218).
+
+%ΔΗΜΙΟΥΡΓΙΑ ΕΝΟΣ ΠΙΘΑΝΟΥ ΚΑΙ ΛΟΓΙΚΟΥ ΠΡΟΓΡΑΜΜΑΤΟΣ ΕΞΕΤΑΣΤΙΚΗΣ ΟΠΟΥ ΤΟ ΙΔΙΟ ΜΑΘΗΜΑ ΔΕΝ ΕΞΕΤΑΖΕΤΑΙ ΠΑΝΩ ΑΠΟ ΜΙΑ ΦΟΡΑ. ΕΛΕΓΧΟΣ ΚΑΘΕ ΜΑΘΗΜΑΤΟΣ ΝΑ ΕΙΝΑΙ ΔΙΑΦΟΡΕΤΙΚΟ ΑΠΤΑ ΥΠΟΛΟΙΠΑ
+schedule([X,Y,Z],[Q,W,E],[J,K]):-subject(X),subject(Y),X\=Y,subject(Z),Z\=Y,Z\=X,subject(Q)
+,Q\=Y,Q\=X,Q\=Z,subject(W),W\=Q,W\=X,W\=Z,W\=Y,subject(E),E\=Q,E\=X,E\=Z,E\=Y,E\=W,subject(J),J\=Q,J\=X,J\=Z,J\=Y,J\=W,J\=E,subject(K),K\=Q,K\=X,K\=Z,K\=Y,K\=W,K\=E,K\=J.
+
+has_subject(X,Y):-student(X),subject(Y),attends(X,Y).
+
+%KANENA MATHIMA TELEUTAIA VDOMADA
+student_score([X,Y],Score_st,Error,W):-(\+has_subject(W,X)),(\+has_subject(W,Y)),Score_st is 0,Error is 0.
+
+%ENA MATHIMA TELEUTAIA VDOMADA/DEUTERA
+student_score([X,Y],Score_st,Error,W):-has_subject(W,X),(\+has_subject(W,Y)),Score_st is 7,Error is 0.
+%ENA MATHIMA TELEUTAIA VDOMADA/TETARTH
+student_score([X,Y],Score_st,Error,W):-(\+has_subject(W,X)),has_subject(W,Y),Score_st is 7,Error is 0.
+
+%DUO MATHIMATA TELEUTAIA VDOMADA
+student_score([X,Y],Score_st,Error,W):-has_subject(W,X),has_subject(W,Y),Score_st is 1,Error is 0.
+
+%KANENA MATHIMA
+student_score([X,Y,Z],Score_st,Error,W):-(\+has_subject(W,X)),(\+has_subject(W,Y)),(\+has_subject(W,Z)),Score_st is 0,Error is 0.
+
+%MONO ENA MATHIMA
+student_score([X,Y,Z],Score_st,Error,W):-has_subject(W,X),(\+has_subject(W,Y)),(\+has_subject(W,Z)),Score_st is 7,Error is 0.
+student_score([X,Y,Z],Score_st,Error,W):-(\+has_subject(W,X)),(\+has_subject(W,Y)),has_subject(W,Z),Score_st is 7,Error is 0.
+student_score([X,Y,Z],Score_st,Error,W):-(\+has_subject(W,X)),has_subject(W,Y),(\+has_subject(W,Z)),Score_st is 7,Error is 0.
+
+%MATHIMATA ME 2 MERES DIAFORA
+student_score([X,Y,Z],Score_st,Error,W):-has_subject(W,X),has_subject(W,Y),(\+has_subject(W,Z)),Score_st is 1,Error is 0.
+student_score([X,Y,Z],Score_st,Error,W):-(\+has_subject(W,X)),has_subject(W,Y),has_subject(W,Z),Score_st is 1,Error is 0.
+
+%DEYTERA-PARASKEVH
+student_score([X,Y,Z],Score_st,Error,W):-has_subject(W,X),(\+has_subject(W,Y)),has_subject(W,Z),Score_st is 3,Error is 0.
+
+%MATHIMA KATHE MERA
+student_score([X,Y,Z],Score_st,Error,W):-has_subject(W,X),has_subject(W,Y),has_subject(W,Z),Score_st is -7,Error is 1.
+
+
+%ΚΩΔΙΚΑΣ ΓΙΑ ΤΟΝ ΥΠΟΛΟΓΙΣΜΟ ΤΟΥ ΣΚΟΡ,RECURSIVE ΒΡΙΣΚΟΥΜΕ ΓΙΑ ΤΟΝ ΚΑΘΕ ΦΟΙΤΗΤΗ ΤΟ ΣΚΟΡ ΤΟΥ ΕΝΑ ΕΝΑ ΚΑΙ ΤΑ ΠΡΟΣΘΕΤΟΥΜΕ ΑΦΟΥ ΤΑ ΒΡΟΥΜΕ ΣΤΟ ΕΒΔΟΜΑΔΙΑΙΟ
+%ΚΩΔΙΚΑΣ ΓΙΑ ΤΗΝ ΕΒΔΟΜΑΔΑ ΜΕ ΔΥΟ ΜΕΡΕΣ
+week_score([_X,_Y],[],Score_week):-Score_week is 0.
+
+week_score([X,Y],[First|Tail],Score_week):-(week_score([X,Y],Tail,Score)),student_score([X,Y],Score_st,_Error,First),plus(Score_st,Score,Score_week).
+
+%ΚΩΔΙΚΑΣ ΓΙΑ ΤΙΣ ΕΒΔΟΜΑΔΕΣ ΜΕ 3 ΜΕΡΕΣ
+week_score([_X,_Y,_Z],[],Score_week):-Score_week is 0.
+
+week_score([X,Y,Z],[First|Tail],Score_week):-(week_score([X,Y,Z],Tail,Score)),student_score([X,Y,Z],Score_st,_Error,First),plus(Score_st,Score,Score_week).
+
+
+
+
+
+
+%ΚΩΔΙΚΑΣ ΓΙΑ ΤΟΝ ΥΠΟΛΟΓΙΣΜΟ ΤΟΥ ΑΡΙΘΜΟΥ ΤΩΝ ΠΡΟΒΛΗΜΑΤΩΝ,RECURSIVE ΒΡΙΣΚΟΥΜΕ ΓΙΑ ΤΟΝ ΚΑΘΕ ΦΟΙΤΗΤΗ ΑΝ ΕΧΕΙ ΠΡΟΒΛΗΜΑ Η ΟΧΙ ΚΑΙ ΠΡΟΣΘΕΤΟΥΜΕ ΑΦΟΥ ΤΑ ΒΡΟΥΜΕ ΣΤΟ ΕΒΔΟΜΑΔΙΑΙΟ
+%ΑΝ ΕΝΑΣ ΦΟΙΤΗΤΗΣ ΕΧΕΙ ΠΡΟΒΛΗΜΑ ΤΟΤΕ ΠΑΙΡΝΕΙ ΤΟΝ ΑΡΙΘΜΟ 1 ΑΛΛΙΩΣ ΤΟΝ ΑΡΙΘΜΟ 0
+week_errors([_X,_Y],[],Errors_week):-Errors_week is 0.
+
+week_errors([X,Y],[First|Tail],Errors_week):-(week_errors([X,Y],Tail,Error_wk)),student_score([X,Y],_Score_st,Error,First),plus(Error_wk,Error,Errors_week).
+
+week_errors([_X,_Y,_Z],[],Errors_week):-Errors_week is 0.
+
+week_errors([X,Y,Z],[First|Tail],Errors_week):-(week_errors([X,Y,Z],Tail,Error_wk)),student_score([X,Y,Z],_Score_st,Error,First),plus(Error_wk,Error,Errors_week).
+
+
+%ΚΩΔΙΚΑΣ ΠΟΥ ΥΠΟΛΟΓΙΖΕΙ ΤΟ ΣΚΟΡ ΓΙΑ ΟΛΕΣ ΤΙΣ ΕΒΔΟΜΑΔΕΣ ΓΙΑ ΕΝΑ ΠΡΟΓΡΑΜΜΑ ΕΞΕΤΑΣΤΙΚΗΣ
+score_schedule([X,Y,Z],[Q,W,E],[J,K],S):-(week_score([X,Y,Z],[476,478,479,480,481,482,483,484,485,486,487,488,489,490,491,492,493,494,495,496,497,498,499,500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515,516,517,518,520],Score_week1),(week_score([Q,W,E],[476,478,479,480,481,482,483,484,485,486,487,488,489,490,491,492,493,494,495,496,497,498,499,500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515,516,517,518,520],Score_week2)),(week_score([J,K],[476,478,479,480,481,482,483,484,485,486,487,488,489,490,491,492,493,494,495,496,497,498,499,500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515,516,517,518,520],Score_week3))),plus(Score_week1,Score_week2,Score_week12),plus(Score_week12,Score_week3,S).
+
+%ΚΩΔΙΚΑΣ ΠΟΥ ΥΠΟΛΟΓΙΖΕΙ ΤΑ ΠΡΟΒΛΗΜΑΤΑ ΓΙΑ ΟΛΕΣ ΤΙΣ ΕΒΔΟΜΑΔΕΣ ΓΙΑ ΕΝΑ ΠΡΟΓΡΑΜΜΑ ΕΞΕΤΑΣΤΙΚΗΣ
+schedule_errors([X,Y,Z],[Q,W,R],[J,K],E):-(week_errors([X,Y,Z],[476,478,479,480,481,482,483,484,485,486,487,488,489,490,491,492,493,494,495,496,497,498,499,500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515,516,517,518,520],Errors_week1),(week_errors([Q,W,R],[476,478,479,480,481,482,483,484,485,486,487,488,489,490,491,492,493,494,495,496,497,498,499,500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515,516,517,518,520],Errors_week2)),(week_errors([J,K],[476,478,479,480,481,482,483,484,485,486,487,488,489,490,491,492,493,494,495,496,497,498,499,500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515,516,517,518,520],Errors_week3))),plus(Errors_week1,Errors_week2,Errors_week12),plus(Errors_week12,Errors_week3,E).
+
+%ΚΩΔΙΚΑΣ ΠΟΥ ΓΙΑ ΚΑΘΕ ΔΥΝΑΤΟ ΠΡΟΓΡΑΜΜΑ ΕΞΕΤΑΣΤΙΚΗΣ ΒΡΙΣΚΕΙ ΤΑ ΠΡΟΒΛΗΜΑΤΑ ΤΟΥ. ΧΡΗΣΙΜΟΠΟΙΕΙΤΑΙ ΣΤΟ ΠΡΟΓΡΑΜΜΑ ΓΙΑ ΝΑ ΒΡΟΥΜΕ ΤΟΝ ΕΛΑΧΙΣΤΟ ΑΡΙΘΜΟ ΠΡΟΒΛΗΜΑΤΩΝ ΚΑΙ ΤΟ ΠΡΟΓΡΑΜΜΑ ΠΟΥ ΑΝΤΙΣΤΟΙΧΕΙ ΣΕ ΑΥΤΟ
+all_errors(A,B,C,E):-schedule(A,B,C),schedule_errors(A,B,C,E).
+
+max1([H|T], Y):-  % with the -> operator, call first
+    max1(T,X),
+    (H > X ->
+     H = Y;
+     Y = X).
+max1([X],X).
+
+list_min1a([X],X).
+list_min1a([X|Tail],M) :-
+	list_min1a(Tail,MT),
+	(X =< MT -> M = X; M = MT).
+
+%ΒΡΙΣΚΟΥΜΕ ΠΡΩΤΑ ΤΟΝ ΑΡΙΘΜΟ ΤΩΝ ΕΛΑΧΙΣΤΩΝ ΠΡΟΒΛΗΜΑΤΩΝ ΠΟΥ ΜΠΟΡΕΙ ΝΑ ΠΡΟΚΥΨΟΥΝ ΚΑΙ ΜΕΤΑ ΚΑΝΟΥΜΕ ΑΝΑΖΗΤΗΣΗ ΓΙΑ ΝΑ ΒΡΟΥΜΕ ΤΑ Α,Β,Γ ΜΕ ΤΟΝ ΣΥΓΚΕΚΡΙΜΕΝΟ ΑΡΙΘΜΟ ΠΡΟΒΛΗΜΑΤΩΝ
+minimal_schedule_errors(A,B,C,E):-findminimumerrors(E),all_errors(A,B,C,E).
+
+%ΒΡΙΣΚΟΥΜΕ ΤΟΝ ΑΡΙΘΜΟ ΟΛΩΝ ΤΟΝ ΠΡΟΒΛΗΜΑΤΩΝ ΠΟΥ ΜΠΟΡΕΙ ΝΑ ΠΡΟΚΥΨΟΥΝ ΚΑΙ ΜΕΤΑ ΒΡΙΣΚΟΥΜΕ ΑΥΤΟ ΜΕ ΤΟΝ ΕΛΑΧΙΣΤΟ ΜΕΤΡΗΤΗ
+findminimumerrors(E):-findall(E,all_errors(_A,_B,_C,E),Score),list_min1a(Score,E).
+
+%ΒΡΙΣΚΟΥΜΕ ΤΟΝ ΑΡΙΘΜΟ ΟΛΩΝ ΤΟΝ ΣΚΟΡ ΠΟΥ ΜΠΟΡΕΙ ΝΑ ΠΡΟΚΥΨΟΥΝ ΚΑΙ ΜΕΤΑ ΒΡΙΣΚΟΥΜΕ ΑΥΤΟ ΜΕ ΤΟΝ ΜΕΓΙΣΤΟ ΜΕΤΡΗΤΗ
+findmaximumscore(Errors,MaxScore):-findall(D,(all_errors(A,B,C,Errors),score_schedule(A,B,C,D)),Score),max1(Score,MaxScore).
+
+%ΤΕΛΙΚΟ ΑΠΟΤΕΛΕΣΜΑ ΠΟΥ ΣΥΝΔΙΑΖΕΙ ΟΛΑ ΤΑ ΠΡΟΗΓΟΥΜΕΝΑ. ΒΡΙΣΚΕΙ ΤΟΝ ΜΙΚΡΟΤΕΡΟ ΑΡΙΘΜΟ ΠΡΟΒΛΗΜΑΤΩΝ ΚΑΙ ΤΟΝ ΑΠΟΘΗΚΕΥΕΙ ΣΤΟ Ε ΜΕΤΑ ΒΡΙΣΚΕΙ ΤΟ ΜΕΓΑΛΥΤΕΡΟ ΣΚΟΡ ΠΟΥ ΑΝΤΙΣΤΟΙΧΕΙ ΣΤΟ ΜΙΚΡΟΤΕΡΟ ΑΡΙΘΜΟ ΠΡΟΒΛΗΜΑΤΩΝ ΚΑΙ ΤΟ ΑΠΟΘΗΚΕΥΕΙ ΣΤΟ S.
+%ΤΕΛΟΣ ΒΡΙΣΚΕΙ ΟΛΑ ΤΑ ΠΡΟΓΡΑΜΜΑΤΑ ΠΟΥ ΕΧΟΥΝ ΣΚΟΡ ΙΣΟ ΜΕ S
+maximum_score_schedule(A,B,C,E,S):-findminimumerrors(E),findmaximumscore(E,S),schedule(A,B,C),score_schedule(A,B,C,S).
+
+
